616e28f7a0a8cd0f5c86dd4abc55f6fe7695dece
Arni Sturluson <sturlusa@oregonstate.edu>
2020-07-28 09:48:20 -0700

    minor improvements to run henry code

--- adsorption_simulations/run_henry.jl
+++ adsorption_simulations/run_henry.jl
@@ -1,9 +1,10 @@
 using PorousMaterials
 
-println(ARGS[1])
-@eval PorousMaterials PATH_TO_CRYSTALS = ARGS[2] * "_xtals"
+charge_assignment_method = ARGS[2]
+@eval PorousMaterials PATH_TO_CRYSTALS = charge_assignment_method * "_xtals"
 
-xtal = Crystal(ARGS[1])
+crystal_name = ARGS[1]
+xtal = Crystal(crystal_name)
 molecule = Molecule("CO2")
 strip_numbers_from_atom_labels!(xtal)
 temp = 298.0
@@ -11,4 +12,4 @@ ljforcefield = LJForceField("UFF", r_cutoff=12.5)
 
 # run the simulation
 result = henry_coefficient(xtal, molecule, temp, ljforcefield,
-					   insertions_per_volume=1000, verbose=true, filename_comment=ARGS[2])
+					   insertions_per_volume=1000, verbose=true, filename_comment=charge_assignment_method)
